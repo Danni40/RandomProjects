820bdde904a71cc52d20ef6a9e1d071734854bf7
Danni40 <stankylew@gmail.com>
2020-03-04 18:57:03 -0600

    To Do List

---
+++ RandomProjects/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string greeting;
+
+	cout << "Please enter your name: ";
+	cin >> greeting;
+	cout << "Hello " << greeting << " You will need to learn to navigate vim.  \n Maybe take some classes";
+}
