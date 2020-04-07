530a87ef511d1f284dcb379c570444961bf6881b
Danni40 <stankylew@gmail.com>
2020-04-06 21:40:14 -0500

    more random projects for google interview

--- RandomProjects/main.cpp
+++ RandomProjects/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include "practice_interview_questions.cpp"
+#include "Duplicates.cpp""
+#include "binaryTreeSumLeafs.cpp"
 
 using namespace std;
 
@@ -6,7 +10,20 @@ int main()
 {
 	string greeting;
 
-	cout << "Please enter your name: ";
-	cin >> greeting;
-	cout << "Hello " << greeting << " You will need to learn to navigate vim.  \n Maybe take some classes";
+	//cout << "Please enter your name: ";
+	//cin >> greeting;
+	//cout << "Hello " << greeting << " You will need to learn to navigate vim.  \n Maybe take some classes" << endl;
+	
+	vector<int> inputNums;
+	vector<int> dups;
+	TreeNode* binaryMode = new TreeNode{NULL};
+	//TreeNode *binaryMode(0);
+	for (int i=0; i<10; i++) inputNums.push_back(i);
+	cout << "max valur after reverse: " << maxValueAfterReverse(inputNums) << endl;
+	dups.push_back(1);
+	dups.push_back(1);
+	dups.push_back(2);
+	cout << "remove duplicates from vector: " << removeDuplicates(dups) << endl;
+
+	cout << "binary tree sum: " << sumRootToLeaf(binaryMode) << endl;
 }
